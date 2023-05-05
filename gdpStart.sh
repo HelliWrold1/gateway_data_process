294e9104403eb30fa7453ab5142b22df76335c68
HelliWrold <albert_yun@qq.com>
2023-05-05 12:00:02 +0800

    添加自启脚本

---
+++ gdpStart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/pi/gateway-data-process/cmake-build-release/
+./gateway_data_process
---
+++ startService.sh
@@ -0,0 +1,3 @@
+sudo cp gateway-data-process.service /usr/lib/systemd/system/gateway-data-process.service 
+sudo systemctl start gateway-data-process.service
+sudo systemctl enable gateway-data-process.service
